3338c710fb9bd42a3b120b1002236ac53cf51be4
Mounir Ait Sliman <105859627+mounirAitsliman@users.noreply.github.com>
2023-06-12 17:20:21 +0100

    innitialised project with spring web and spring data jpa dependencies

---
+++ src/main/java/ma/ensa/meeting_planner/MeetingPlannerApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.meeting_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ensa/meeting_planner/MeetingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.meeting_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
